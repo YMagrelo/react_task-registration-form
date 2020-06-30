992676d667893d708b960efc8eb391fd61a5e232
Yaroslav Magrelo <slava.magrelo@gmail.com>
2020-06-30 21:44:11 +0300

    added About section, made header fixed, linked fonts

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,12 @@ import React from 'react';
 import './App.scss';
 import './resets.scss';
 import { Header } from './components/Header';
+import { About } from './components/About';
 
 const App = props => (
   <div className="app-wrapper">
     <Header />
+    <About />
   </div>
 );
 
---
+++ src/components/About.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './About.scss';
+import backgroundBanner from '../assets/image/banner-photo.jpg';
+
+export const About = () => (
+  <section className="about">
+    <div></div>
+  </section>
+);
+
+
+//<img src={backgroundBanner} alt="about us banner" />
