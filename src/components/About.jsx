48310615759930284e6330ed6a7455df92a9fac7
Yaroslav Magrelo <slava.magrelo@gmail.com>
2020-07-01 17:08:02 +0300

    added responsive design for max-screen 360 to Header and About

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -10,10 +10,12 @@ export const About = () => (
       <p className="content__text">
         We kindly remind you that your test assignment should
         be submitted as a link to github/bitbucket repository.
+        <div className="content__text content__text--whole">
         Please be patient, we consider and respond to every
         application that meets minimum requirements. We look
         forward to your submission. Good luck! The photo has to
         scale in the banner area on the different screens
+        </div>
       </p>
       <button className="content__button" type="button">Sing up now</button>
     </div>
